41076f7b4269f87ff81c8e8efcce038359615581
dobashi.kentarou <dobakenjp@gmail.com>
2026-02-19 15:50:49 +0900

    セキュリティ修正: handle_updated_at関数にsearch_path設定を追加

--- supabase/migrations/20260203_create_profiles.sql
+++ supabase/migrations/20260203_create_profiles.sql
@@ -94,6 +94,7 @@ ON CONFLICT (id) DO NOTHING;
 CREATE OR REPLACE FUNCTION public.handle_updated_at()
 RETURNS TRIGGER
 LANGUAGE plpgsql
+SET search_path = public
 AS $$
 BEGIN
     NEW.updated_at = timezone('utc'::text, now());
